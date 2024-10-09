b71a1c56cb8fb249ce238c3b0c5df2b584002098
Lamija <lamija2002@gmail.com>
2024-10-09 21:43:56 +0200

    Add: Navbar Component

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div className="w-full bg-gradient-to-r from-indigo-500 to-purple-500 text-white p-4 flex justify-between items-center">
+      <div className="font-bold text-xl ml-[25px]">logo</div>
+
+      <ul className="flex space-x-[35px] mr-[25px]">
+        <li><a href="#game" className="hover:underline">Game</a></li>
+        <li><a href="#leaderboard" className="hover:underline">Leaderboard</a></li>
+        <li><a href="#mindbux" className="hover:underline">Mindbux</a></li>
+        <li><a href="https://www.klix.ba/" target="_blank" className="hover:underline">Blog</a></li>
+        <li><a href="#about" className="hover:underline">About Us</a></li>
+        <li><a href="#login" className="hover:underline">Log In</a></li>
+        <li><a href="#signup" className="hover:underline">Sign Up</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
