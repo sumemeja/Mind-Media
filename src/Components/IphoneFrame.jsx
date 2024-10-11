0eda61b9d1fb356f1e4de5fe51d8602f279fc2cf
Lamija <lamija2002@gmail.com>
2024-10-11 22:57:12 +0200

    Update: Iphone frame coloring

--- src/Components/IphoneFrame.jsx
+++ src/Components/IphoneFrame.jsx
@@ -2,11 +2,11 @@ import Video from "/tiktok.mp4";
 
 export default function IphoneFrame() {
   return (
-    <div className="relative w-[305px] h-[585px] bg-transparent border-[7px] border-[#5e17eb] rounded-[40px] overflow-hidden">
+    <div className="relative w-[305px] h-[585px] bg-gradient-to-r from-[#37271d] to-[#bb642e] border-[5px] border-black rounded-[40px] overflow-hidden">
       
       {/* Iphone small black thing at the top */}
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[90px] bg-transparent">
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[137px] bg-[#5e17eb] rounded-b-[17px]"></div>
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[137px] bg-gradient-to-r from-[#37271d] to-[#bb642e] rounded-b-[17px] border-b-[5px] border-l-[5px] border-r-[5px] border-black"></div>
       </div>
       
       <video 
