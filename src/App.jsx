d3e433613a2f05ae6e02c3149f54298e1c999eef
Lamija <lamija2002@gmail.com>
2024-10-11 15:10:31 +0200

    Update: Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,37 @@ export default function App() {
       <div>
         <Navbar />
 
-        <div className="pl-[150px] pt-[45px] pb-[45px] flex items-start">
-          <IphoneFrame />
+        <div className="pl-[11%] pt-[4%] pb-[3%] flex flex-col lg:flex-row items-start lg:items-center">
+          <div className="flex justify-center w-full lg:w-auto">
+            <IphoneFrame />
+          </div>
 
           {/* Title */}
           <img
-            className="inline-block ml-[125px]"
+            className="inline-block lg:ml-[8%] lg:mt-[-150px] w-[90%] lg:w-[60%] xl:w-[60%]"
             src={TitleImage}
             alt="Title"
-            width={665}
           />
 
           {/* Buttons */}
-          <div className="absolute top-[525px] left-[905px] transform -translate-x-1/2 flex space-x-4">
-            <button className="bg-[#5e17eb] text-[21px] text-white px-[130px] py-[11px] rounded-md hover:bg-gray-800">Play</button>
-            <button className="bg-[#5e17eb] text-[21px] text-white px-[130px] py-[11px] rounded-md hover:bg-gray-800">Learn</button>
+          <div className="relative mt-6 lg:absolute lg:top-[30%] lg:left-[67%] flex space-x-4 transform lg:-translate-x-1/2">
+            <button className="bg-[#5e17eb] text-base lg:text-[21px] text-white px-[15%] lg:px-[130px] py-[3%] rounded-md hover:bg-gray-800">
+              Play
+            </button>
+            <button className="bg-[#5e17eb] text-base lg:text-[21px] text-white px-[15%] lg:px-[130px] py-[3%] rounded-md hover:bg-gray-800">
+              Learn
+            </button>
           </div>
         </div>
       </div>
 
       {/* Leaderboard Component */}
-      <div id="leaderboard" className="relative mt-[85px] pb-[55px]">
+      <div id="leaderboard" className="relative mt-[85px] pb-[55px] px-[5%]">
         <Leaderboard />
       </div>
 
       {/* About Us Component */}
-      <div id="aboutus" className="relative mt-[85px] pb-[55px]">
+      <div id="aboutus" className="relative mt-[85px] pb-[55px] px-[5%]">
         <AboutUs/>
       </div>
     </div>
