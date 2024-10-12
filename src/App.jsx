f197abe861110ec9bdb6bce8e200c2446dd1434e
Lamija <lamija2002@gmail.com>
2024-10-12 21:08:27 +0200

    Update: Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Blog from './Pages/Blog';
 
 function App() {
   return (
-    <Router basename="/Mind-Media">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
