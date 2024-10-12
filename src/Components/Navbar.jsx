5bee7e370178d13fb0b6ed24536fbc9a91e25d73
Lamija <lamija2002@gmail.com>
2024-10-12 21:09:17 +0200

    Update: Routings and fixed Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <div className="w-full bg-gradient-to-r from-[#37271d] to-[#bb642e] text-white p-4 flex justify-between items-center">
-      <div className="font-bold text-xl ml-[25px]">logo</div>
+    <div className="fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-[#37271d] to-[#bb642e] text-white p-4 flex justify-between items-center">
+
+      <Link to="/">
+        <div className="font-bold text-xl ml-[25px]">logo</div>
+      </Link>
 
       <ul className="flex space-x-[35px] mr-[25px]">
         <li><a href="#game" className="hover:underline">Game</a></li>
         <li><a href="#leaderboard" className="hover:underline">Leaderboard</a></li>
         <li><a href="#mindbux" className="hover:underline">Mindbux</a></li>
-        <li><a href="https://www.klix.ba/" target="_blank" className="hover:underline">Blog</a></li>
+        <Link to="/blog">
+          <p className="hover:underline">Blog</p>
+        </Link>
         <li><a href="#aboutus" className="hover:underline">About Us</a></li>
         <li><a href="#login" className="hover:underline">Log In</a></li>
         <li><a href="#signup" className="hover:underline">Sign Up</a></li>
