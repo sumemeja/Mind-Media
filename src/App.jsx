c07c8d4348a1320f8175eb0ae5e17c69b6337b0a
Lamija <lamija2002@gmail.com>
2024-10-21 21:28:29 +0200

    Add: Scroll to top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Blog from './Pages/Blog';
+import ScrolltoTop from './Components/ScrolltoTop';
 
 function App() {
   return (
     <Router basename="/Mind-Media">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
+      <ScrolltoTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </ScrolltoTop>
     </Router>
   );
 }
