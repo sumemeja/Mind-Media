98940050508396b3c64adf4528e76035b9fc85f1
Lamija <lamija2002@gmail.com>
2024-10-21 19:56:30 +0200

    Update: Google Drive Video

--- src/Components/IphoneFrame.jsx
+++ src/Components/IphoneFrame.jsx
@@ -1,5 +1,3 @@
-import Video from "/final.mp4";
-
 export default function IphoneFrame() {
   return (
     <div className="relative w-[305px] h-[585px] bg-gradient-to-r from-[#37271d] to-[#bb642e] border-[5px] border-black rounded-[40px] overflow-hidden">
@@ -8,13 +6,12 @@ export default function IphoneFrame() {
       <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[90px] bg-transparent">
         <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[137px] bg-gradient-to-r from-[#37271d] to-[#bb642e] rounded-b-[17px] border-b-[5px] border-l-[5px] border-r-[5px] border-black"></div>
       </div>
-      
-      <video 
-        autoPlay 
-        loop 
-        src={Video} 
-        controls 
-        className="absolute w-[95%] h-[85%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover"
+
+      {/* Embed Google Drive Video using iframe */}
+      <iframe 
+        src="https://drive.google.com/file/d/1V30_pPpp5-j4c1Oy7Bpj5PkGBItOSiX8/preview" 
+        allow="autoplay" 
+        className="absolute w-[94%] h-[85%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
       />
     </div>
   );
