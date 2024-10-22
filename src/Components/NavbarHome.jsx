2d517d5fcf2577e7113be8eaeab2fe26378c3f9d
Lamija <lamija2002@gmail.com>
2024-10-22 22:43:10 +0200

    Add: Logo.png in Navbar and NavbarHome

--- src/Components/NavbarHome.jsx
+++ src/Components/NavbarHome.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png';
 
 const NavbarHome = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-[#37271d] to-[#bb642e] text-white p-4 flex justify-between items-center">
 
       <Link to="/">
-        <div className="font-bold text-xl ml-[25px]">logo</div>
+        <div className="flex items-center ml-[10px] transform transition-transform hover:scale-105">
+          <img src={Logo} alt="Logo" className="w-[30px] h-[30px]" />
+          <p className="ml-2 font-bold text-xl">MIND MEDIA</p>
+        </div>
       </Link>
 
       <ul className="flex space-x-[35px] mr-[25px]">
