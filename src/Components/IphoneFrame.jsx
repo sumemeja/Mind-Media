20a02f7cbfc41a0b2dec9fc5bfeb28918d43ffa7
Lamija <lamija2002@gmail.com>
2024-10-22 23:13:53 +0200

    Add: Video credits

--- src/Components/IphoneFrame.jsx
+++ src/Components/IphoneFrame.jsx
@@ -1,18 +1,26 @@
 export default function IphoneFrame() {
   return (
-    <div className="relative w-[305px] h-[585px] bg-gradient-to-r from-[#37271d] to-[#bb642e] border-[5px] border-black rounded-[40px] overflow-hidden">
-      
-      {/* Iphone small black thing at the top */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[90px] bg-transparent">
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[137px] bg-gradient-to-r from-[#37271d] to-[#bb642e] rounded-b-[17px] border-b-[5px] border-l-[5px] border-r-[5px] border-black"></div>
+    <div className="flex flex-col items-center">
+      {/* Iphone frame */}
+      <div className="relative w-[305px] h-[585px] bg-gradient-to-r from-[#37271d] to-[#bb642e] border-[5px] border-black rounded-[40px] overflow-hidden">
+        
+        {/* Iphone small black thing at the top */}
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[90px] bg-transparent">
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 h-[25px] w-[137px] bg-gradient-to-r from-[#37271d] to-[#bb642e] rounded-b-[17px] border-b-[5px] border-l-[5px] border-r-[5px] border-black"></div>
+        </div>
+
+        {/* Embed Google Drive Video using iframe */}
+        <iframe 
+          src="https://drive.google.com/file/d/1V30_pPpp5-j4c1Oy7Bpj5PkGBItOSiX8/preview" 
+          allow="autoplay" 
+          className="absolute w-[94%] h-[85%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
       </div>
 
-      {/* Embed Google Drive Video using iframe */}
-      <iframe 
-        src="https://drive.google.com/file/d/1V30_pPpp5-j4c1Oy7Bpj5PkGBItOSiX8/preview" 
-        allow="autoplay" 
-        className="absolute w-[94%] h-[85%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-      />
+      {/* Credits */}
+      <p className="mt-4 text-sm text-[#f6d4be] text-center font-mono">
+        Video: Hamza Selimovic, Cast: Kadir Kopic
+      </p>
     </div>
   );
 }
